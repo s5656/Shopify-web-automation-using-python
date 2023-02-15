517178e657a515ed459e7db586d21ad5caf1e0b2
Sumit <testvagrant@TestVagrants-MacBook-Pro.local>
2023-02-15 11:21:31 +0530

    Update

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from selenium import webdriver
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from selenium.webdriver.common.by import By
 
+driver = webdriver.Chrome()
+driver.maximize_window()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+driver.get("https://testvagrant.myshopify.com/")
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
